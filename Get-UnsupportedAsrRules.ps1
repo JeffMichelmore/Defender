cf7e8d9f32e7d7fb4655c0877ae54e44f9de90c8
Jeff Michelmore <Jmichelmore@microsoft.com>
2024-02-12 11:07:18 -0600

    Updated to check for property value instead of log message for scenarios where script is ran on machine without SenseCM driver.

--- Get-UnsupportedAsrRules.ps1
+++ Get-UnsupportedAsrRules.ps1
@@ -12,7 +12,7 @@ function Get-UnsupportedAsrRule {
     foreach ($log in $senseLog){
         # check if $log.message contains an unsupported ASR rule
         $regexPattern = '[a-f\d]{8}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{12}'
-        if ($log.message -like '*SENSECM: WRN: ASR: at least one requested rule is not supported by this platform*' -and $log.Message -match $regexPattern){
+        if ($log.properties.value -like '*SENSECM: WRN: ASR: at least one requested rule is not supported by this platform*' -and $log.properties.value -match $regexPattern){
             $affectedRule = $matches[0]
             
             if ($affectedRules -notcontains $affectedRule) {
@@ -32,3 +32,17 @@ function Get-UnsupportedAsrRule {
     }
 }
 Get-UnsupportedAsrRule
+
+
+<#
+$senseLogPath = [System.Environment]::ExpandEnvironmentVariables("%SystemRoot%\System32\Winevt\Logs\Microsoft-Windows-SENSE%4Operational.evtx")
+$senseLog = Get-WinEvent -Path $senseLogPath -FilterXPath "*[System/EventID=2001]"
+$regexPattern = '[a-f\d]{8}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{12}'
+foreach ($event in $senseLog) {
+    $properties = $event.properties
+    if ($properties.value -match $regexPattern){
+        $properties
+    }
+    
+}
+#>
